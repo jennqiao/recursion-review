203a34fb1e5b0b9f8d8fa506b70d32448e27411c
jennqiao <jennifercqiao@gmail.com>
2018-03-05 16:29:29 -0800

    finished getelements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,38 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+// You should use document.body, element.childNodes, and element.classList
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var initNode = document.body;
+  return getElementsInternal(initNode);
+
+  function getElementsInternal(node){
+    var classArray = [];
+
+    if (node.classList && Array.prototype.includes.call(node.classList, className)) {
+      classArray.push(node);
+    }
+
+    if (node.childNodes.length > 0) {
+      
+      for (var i=0; i< node.childNodes.length; i++) {
+              
+        classArray = classArray.concat(getElementsInternal(node.childNodes[i])); 
+      }
+      
+    
+    }
+
+      return classArray;
+
+  }
+  //base case: if it doesn't have a child node and has the class name, add it to the array
+ 
+  
+  //check if it has child nodes, 
+  //if it matches the classname, return it
+  //if so, for each of its childnodes, getelementsbyclassname
+
+  
 };
